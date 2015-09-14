500bb272363ab9e4cb77707907218c21df5d0f11
Serhii Kopach <Serega@MacBook-Air.local>
2015-09-14 09:32:48 +0300

    Initial Commit

---
+++ NumeralSystemPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumeralSystemPlus
+//
+//  Created by Serhii Kopach on 14.09.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
