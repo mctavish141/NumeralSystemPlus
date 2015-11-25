33b93b1251ff11778073bcccf780247a6ca0993c
mctavish141 <serega.kiev.ua@gmail.com>
2015-11-25 20:58:49 +0200

    Update and rename class.c to class.m

---
+++ class.m
@@ -0,0 +1,11 @@
+class Computer {
+    public void PressTurnOnButton {
+        self.TurnOnCoolers ();
+        self.TurnOnDrives ();
+        self.LoadBIOS ();
+    }
+    
+    private void TurnOnCoolers ();
+    private void TurnOnDrives ();
+    private void LoadBIOS ();
+}
