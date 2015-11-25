2ab11a618c1eb22d7463269c68c4a39aea55ab0a
mctavish141 <serega.kiev.ua@gmail.com>
2015-11-25 22:14:05 +0200

    Update and rename city.c to city.m

---
+++ city.m
@@ -0,0 +1,11 @@
+class City {
+    string name;
+    int population;
+}
+
+City city1 = new City ("Kyiv", 2804000);
+City city2 = new City ("Kharkiv", 1431000);
+City city3 = new City ("Melitopol", 156000);
+
+/*City cities[] = {new City (name: "Kyiv", population: 3000000),
+                 new City (name: "Kharkiv", population: 1500000)};*/
