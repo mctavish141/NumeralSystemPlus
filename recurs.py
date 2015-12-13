19f239d9895f8d70d3effdbaf532ee6ed0f9b7d7
mctavish141 <serega.kiev.ua@gmail.com>
2015-12-13 20:22:46 +0200

    Update recurs.py

--- recurs.py
+++ recurs.py
@@ -1,1 +1,13 @@
+import string
 
+def recurs(str,i):
+    if (i==len(str)-1):
+        return min(str[0],str[i])
+    else:
+        return recurs(str,i+1)
+
+str = raw_input('Enter the string: ')
+if str[0] in string.hexdigits and str[-1] in string.hexdigits:
+    print 'Smaller is',recurs(string.lower(str),0)
+else:
+    print 'Not hexadecimal numbers'
